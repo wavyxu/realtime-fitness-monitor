4d80ef4ff97429a627c686a17bc808aadeee6bbc
Wei Xu <wavyxu@gmail.com>
2018-10-14 16:19:20 -0700

    Fixed bug - sidebar didn't show

--- src/app/home.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: '<h1>The Home Component</h1>'
-})
-export class HomeComponent {}
--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,32 +1,20 @@
-import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-
-import { AuthService } from '../../auth/auth.service';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
-  isAuth = false;
-  authSubscription: Subscription;
 
-  constructor(private authService: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
-      this.isAuth = authStatus;
-    });
   }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
-  }
-
-  ngOnDestroy() {
-    this.authSubscription.unsubscribe();
   }
 
 }
